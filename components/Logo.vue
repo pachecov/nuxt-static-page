a51b42469a2499952d2f88d54416827de5904bbf
Virgilio Pacheco Choque <pachecochoque@gmail.com>
2020-06-07 20:51:19 -0400

    make it better

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="https://vueschool.io/img/favicons/apple-touch-icon.png" alt="the logo">
+</template>
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+          id: 'balut',
+          title: 'What is balut?',
+          content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eaque enim eos magni quae libero nam, quod debitis commodi fugit vero nisi, et iure a officiis dolores officia epostplicabo! Velit'
+        },
+        {
+          id: 'whereIsIt',
+          title: 'Where is the sing in button?',
+          content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eaque enim eos magni quae libero nam, quod debitis commodi fugit vero nisi, et iure a officiis dolores officia epostplicabo! Velit'
+        },
+        {
+          id: 'how',
+          title: 'How Can I fipost the problem?',
+          content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eaque enim eos magni quae libero nam, quod debitis commodi fugit vero nisi, et iure a officiis dolores officia epostplicabo! Velit'
+        }
+      ]
+});
